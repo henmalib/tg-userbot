160e9abacb842908518f43b4cbe7f12cd756f91b
henmalib <simidzu2ay@protonmail.com>
2025-12-23 23:04:15 +0100

    init

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+    stylistic: {
+        indent: 4,
+    },
+    typescript: true,
+    yaml: false,
+    rules: {
+        'curly': ['error', 'multi-line'],
+        'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+        'style/quotes': ['error', 'single', { avoidEscape: true }],
+        'antfu/if-newline': 'off',
+        'style/max-statements-per-line': ['error', { max: 2 }],
+        'no-console': 'off',
+        'node/prefer-global/process': 'off',
+        'antfu/no-top-level-await': 'off',
+    },
+})
